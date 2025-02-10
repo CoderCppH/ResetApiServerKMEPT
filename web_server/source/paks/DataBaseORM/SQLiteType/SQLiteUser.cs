2759ee6405485e73ca64f66889f8ca079fc33b49
codercpph <coder.cpp.h@gmail.com>
2025-02-10 21:57:15 +0700

    add database and empty files web

---
+++ web_server/source/paks/DataBaseORM/SQLiteType/SQLiteUser.cs
@@ -0,0 +1,55 @@
+using System.Data.SQLite;
+
+namespace web_server{
+    class SQLiteUser{
+        //name
+        public static string @nameTable = "user_data";
+        public static string @id = "@id";
+        public static string @fullname = "@fullname";
+        public static string @email = "@email";
+        public static string @password  = "@password";
+        //value
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set;} = string.Empty;
+
+        public static DataBaseConnection database = new DataBaseConnection();
+        public static void CreateTable(){
+            string sqlCommand = $"CREATE TABLE IF NOT EXISTS {@nameTable} ( id INTEGER PRIMARY KEY AUTOINCREMENT, fullname TEXT NOT NULL, email TEXT NOT NULL, password TEXT NOT NULL )";
+            using(SQLiteCommand command = new SQLiteCommand(sqlCommand, database.GetConnection()))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+        public bool Create()
+        {
+            try{
+                string sqlCommand = $"INSERT INTO {@nameTable} (fullname, email, password) VALUES ({@fullname}, {@email}, {@password})";
+                using(SQLiteCommand  command = new SQLiteCommand(sqlCommand, database.GetConnection()))
+                {
+                    command.Parameters.AddWithValue(@fullname, FullName);
+                    command.Parameters.AddWithValue(@email, Email);
+                    command.Parameters.AddWithValue(@password, Password);
+                    command.ExecuteNonQuery();
+                }
+                return true;
+            }
+            catch{
+                return false;
+            }
+        }
+        public static List<SQLiteUser> GetUsers()
+        {
+            List<SQLiteUser> users = new List<SQLiteUser>();
+            string sqlCommand = $"SELECT * FROM {@nameTable}";
+            using(SQLiteCommand command = new SQLiteCommand(sqlCommand, database.GetConnection()))
+            {
+                var read = command.ExecuteReader();
+                while(read.Read())
+                    users.Add(new SQLiteUser(){Id = read.GetInt32(0), FullName = read.GetString(1), Email = read.GetString(2), Password = read.GetString(3)});
+            }
+            return users;
+        }
+    }
+}
