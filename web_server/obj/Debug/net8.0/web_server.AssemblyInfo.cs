0096c2de784f08a29fc15e4b163535b1a20325d5
codercpph <coder.cpp.h@gmail.com>
2025-03-02 15:01:04 +0300

    mod exe file

--- web_server/obj/Debug/net8.0/web_server.AssemblyInfo.cs
+++ web_server/obj/Debug/net8.0/web_server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web_server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a98b3a525b46337a73fc969c39aac4490a909f87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55d39d4a4043788a25b2d8dfee2021fb2e989c05")]
 [assembly: System.Reflection.AssemblyProductAttribute("web_server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web_server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
