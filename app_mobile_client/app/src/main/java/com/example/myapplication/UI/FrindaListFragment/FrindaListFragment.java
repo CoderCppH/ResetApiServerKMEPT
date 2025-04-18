3435e8acd51d779c48099d14c153c29448b6abc0
pag6666 <pargev20002607@gmail.com>
2025-04-19 00:28:55 +0300

    add fun 'AddItem' for all Adapter for recycleView

--- app_mobile_client/app/src/main/java/com/example/myapplication/UI/FrindaListFragment/FrindaListFragment.java
+++ app_mobile_client/app/src/main/java/com/example/myapplication/UI/FrindaListFragment/FrindaListFragment.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.myapplication.LIST_USER.UserAdapter;
 import com.example.myapplication.LIST_USER.p_user_item;
@@ -30,9 +31,20 @@ public class FrindaListFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         adapter = new UserAdapter(inflater, getData());
         recyclerView.setAdapter(adapter);
+        Init(view);
         return view;
     }
 
+    private void Init(View view) {
+        Button btn = view.findViewById(R.id.btn_add_friend);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                p_user_item item = new p_user_item(R.drawable.ic_launcher_foreground, "Pargev", "coder.cpp.h@gmail.com");
+                adapter.AddItem(item);
+            }
+        });
+    }
     private List<p_user_item> getData() {
         ArrayList<p_user_item> list = new ArrayList<p_user_item>();
         p_user_item item = new p_user_item(R.drawable.ic_launcher_foreground, "admin", "Anton234@gmail.com");
